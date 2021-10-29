cc92e9b4449b6df4b7581bdfa8eb298ebbee77e0
patrick <patrick@o-neill.ca>
2021-10-29 15:27:18 -0400

    added removeFromArray function, passes first npm test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(givenArray, valueToRemove) {
+  let myArray = givenArray;
+  let index = myArray.indexOf(valueToRemove);
+  myArray.splice(index, 1);
+  return myArray;
 };
 
 // Do not edit below this line
