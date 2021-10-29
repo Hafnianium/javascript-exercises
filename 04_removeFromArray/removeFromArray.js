d7efba71a8fa0ea6c3c09ea9a6f684a71c6ed701
patrick <patrick@o-neill.ca>
2021-10-29 18:53:30 -0400

    Works with invalid inexes

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -5,9 +5,17 @@ const removeFromArray = function(givenArray, ...valuesToRemove) {
   for (i = 0; i < removeArray.length; i++) {
     indexes.push(myArray.indexOf(removeArray[i]))
   }
-  
-  for (i = 0; i < indexes.length; i++) {
-    myArray.splice(indexes[i], 1);
+
+  for (i = 0; i < indexes.length; i++) { //Checks there are no invalid indexes present
+    if (indexes[i] == -1) {
+      indexes.splice(i, 1)
+    }
+  }
+
+  if (indexes != -1) {
+    for (i = 0; i < indexes.length; i++) {
+      myArray.splice(indexes[i], 1);
+    }
   }
   
   return myArray;
