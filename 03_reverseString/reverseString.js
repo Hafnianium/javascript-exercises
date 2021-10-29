1c606f4a439ffc0069ae4bda2ddad80fed8e4d61
patrick <patrick@o-neill.ca>
2021-10-29 15:05:36 -0400

    Setup reverseString function and passed all spec tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
 const reverseString = function(str) {
-
+  let stringList = str.split("")
+  let stringArray = Array.from(stringList)
+  let reversedArray = stringArray.reverse()
+  let reversedString = reversedArray.join("")
+  return reversedString
 };
 
 // Do not edit below this line
