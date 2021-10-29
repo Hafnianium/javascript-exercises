619630981432819582cf5b9d3c606ba47b9a13c6
patrick <patrick@o-neill.ca>
2021-10-29 14:06:00 -0400

    all npm tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,17 @@
 const repeatString = function(x, y) {
 let repeatedString = x
+if (y == 0) {
+  return ""
+} else if (y < 0) {
+  return "ERROR"
+} else {
   for (i = 0; i < y -1; i++) {
     repeatedString = repeatedString.concat(x);
+  }
+  return repeatedString;
 }
-return repeatedString;
-};
+}
+  
 
 // Do not edit below this line
 module.exports = repeatString;
