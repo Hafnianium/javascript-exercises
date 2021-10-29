70dd7c94c5dc729d48248235f3d2705d0e1fd507
patrick <patrick@o-neill.ca>
2021-10-29 13:48:21 -0400

    Passed first js test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(x, y) {
+let repeatedString = x
+  for (i = 0; i < y -1; i++) {
+    repeatedString = repeatedString.concat(x);
+}
+return repeatedString;
 };
 
 // Do not edit below this line
