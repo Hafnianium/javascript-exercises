6602a9b4719ce71cfdefa4295955799ddf9f7d8e
patrick <patrick@o-neill.ca>
2021-10-29 22:13:48 -0400

    Re-did function, all tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,25 +1,13 @@
 const removeFromArray = function(givenArray, ...valuesToRemove) {
   let myArray = givenArray;
-  let removeArray = valuesToRemove
-  let indexes = []
-  for (i = 0; i < removeArray.length; i++) {
-    indexes.push(myArray.indexOf(removeArray[i]))
-  }
-
-  for (i = 0; i < indexes.length; i++) { //Checks there are no invalid indexes present
-    if (indexes[i] == -1) {
-      indexes.splice(i, 1)
+  for (let i = 0; i < valuesToRemove.length; i++) {
+    let equalArrayElement = element => element === valuesToRemove[i];
+    let indexOfValuesToRemove = myArray.findIndex(equalArrayElement);
+    if (indexOfValuesToRemove != -1) {
+      myArray.splice(indexOfValuesToRemove, 1);
     }
   }
-
-  if (indexes != -1) {
-    for (i = 0; i < indexes.length; i++) {
-      myArray.splice(indexes[i], 1);
-    }
-  }
-  
-  return myArray;
-  
+  return myArray
 };
 
 // Do not edit below this line
