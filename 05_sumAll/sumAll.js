fd3dcc681ea23f4a4a13bb36e268bda7600baf3d
patrick <patrick@o-neill.ca>
2021-10-29 22:57:19 -0400

    changed if statements to use an abs value

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,27 +1,15 @@
 const sumAll = function(firstNumber, secondNumber) {
-  if (firstNumber > secondNumber) {
-    let differenceBetween = firstNumber - secondNumber
+  if (firstNumber == secondNumber) {
+    return firstNumber + secondNumber
+  } else {
+    let differenceBetween = Math.abs(firstNumber - secondNumber)
     let sequentialSum = 0
     for (i = 0; i < differenceBetween; i++) {
-      sequentialSum += i
+      sequentialSum += (differenceBetween - i)
     }
-    sequentialSum += firstNumber + secondNumber - 1
+    sequentialSum += (firstNumber + secondNumber -1)
     return sequentialSum
-  } else if (firstNumber < secondNumber) {
-      let differenceBetween = secondNumber - firstNumber
-      let sequentialSum = 0
-      for (i = 0; i < differenceBetween + 1; i++) {
-        sequentialSum += i
-    }
-    sequentialSum += firstNumber + secondNumber - 1
-    return sequentialSum
-  } else if (firstNumber = secondNumber) {
-      let sequentialSum = 0
-      sequentialSum += firstNumber + secondNumber
-      return sequentialSum
   }
-  
-  
 };
 
 // Do not edit below this line
