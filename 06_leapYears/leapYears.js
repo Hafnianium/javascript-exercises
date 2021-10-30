e6d4fad8ed9e12e35ee3618567bf625397333edb
patrick <patrick@o-neill.ca>
2021-10-29 23:41:09 -0400

    leaYear function created and tested for all scenarios

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(possibleLeapYear) {
+  if (possibleLeapYear % 4 == 0 && possibleLeapYear % 400 == 0 && possibleLeapYear % 100 == 0) {
+    return true
+  } else if (possibleLeapYear % 4 == 0 && possibleLeapYear % 100 == 0) {
+    return false
+  } else if (possibleLeapYear % 4 == 0 && possibleLeapYear % 100 != 0) {
+    return true
+  } else {
+    return false
+  }
 };
 
 // Do not edit below this line
