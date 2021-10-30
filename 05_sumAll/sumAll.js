6c3159ecede431a45387e56bfb6f10be61b60adf
patrick <patrick@o-neill.ca>
2021-10-29 22:47:45 -0400

    First two tests passed but pretty wonky code, need to reduce number of if statements

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+  if (firstNumber > secondNumber) {
+    let differenceBetween = firstNumber - secondNumber
+    let sequentialSum = 0
+    for (i = 0; i < differenceBetween; i++) {
+      sequentialSum += i
+    }
+    sequentialSum += firstNumber + secondNumber - 1
+    return sequentialSum
+  } else if (firstNumber < secondNumber) {
+      let differenceBetween = secondNumber - firstNumber
+      let sequentialSum = 0
+      for (i = 0; i < differenceBetween + 1; i++) {
+        sequentialSum += i
+    }
+    sequentialSum += firstNumber + secondNumber - 1
+    return sequentialSum
+  } else if (firstNumber = secondNumber) {
+      let sequentialSum = 0
+      sequentialSum += firstNumber + secondNumber
+      return sequentialSum
+  }
+  
+  
 };
 
 // Do not edit below this line
