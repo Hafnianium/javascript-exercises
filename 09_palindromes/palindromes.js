11ca340b0c9fe2fb8eb37b70ae73461287b24718
patrick <patrick@o-neill.ca>
2021-11-01 15:12:13 -0400

    completed palindrome detector exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (possiblePalindrome) {
+  let punctuationRemoved = possiblePalindrome.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+  let finalString = punctuationRemoved.replace(/\s{2,}/g," ");
+  let spacesRemoved = finalString.replace(/\s/g, '');
+  let normalArray = spacesRemoved.toLowerCase().split('');
+  let reverseArray = [...normalArray]
+  reverseArray.reverse()
+  let normalString = normalArray.join()
+  let reverseString = reverseArray.join()
+  if (normalString == reverseString) {
+    return true
+  } else {
+    return false
+  }
 };
 
 // Do not edit below this line
