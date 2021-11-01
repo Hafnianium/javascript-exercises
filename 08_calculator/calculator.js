3a6665d15fd2ae8a7a84bb1942e52715e686fec1
patrick <patrick@o-neill.ca>
2021-11-01 14:47:12 -0400

    setup calculator functions passed all spec

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y
 };
 
-const sum = function() {
-	
+const sum = function(x) {
+	mySum = 0
+  for (i = 0; i < x.length; i++) {
+    mySum += x[i]
+  }
+  return mySum
 };
 
-const multiply = function() {
-
+const multiply = function(x) {
+  myProduct = 1
+  for (i = 0; i < x.length; i++) {
+    myProduct *= x[i]
+  }
+  return myProduct
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  return x**y
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	myFactorial = 1
+  for (i = x; i > 0; i--) {
+    myFactorial *= i
+  }
+  return myFactorial
 };
 
 // Do not edit below this line
