5f8373ad2750855331d0340cfa9632c354ae1346
patrick <patrick@o-neill.ca>
2021-11-01 15:45:19 -0400

    fibonacci sequence completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+  if (x < 0) {
+    return "OOPS"
+  }
+  if (x == 1) {
+    return 1
+  } else if (x == 2) {
+    return 1
+  } else {
+      let fibonacciSequence = [0, 1, 1]
+      for (i = 3; i < x + 1; i++) {
+        fibonacciSequence[i] = fibonacciSequence[i-1] + fibonacciSequence[i-2]
+      }
+    return fibonacciSequence[x]
+  }
 };
 
 // Do not edit below this line
